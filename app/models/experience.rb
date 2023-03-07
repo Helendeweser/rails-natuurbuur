e1cb5672ac0714cf49433a0a0ae754ad50563e78
Florian de Radiguès <florian.radigues@gmail.com>
2023-03-07 15:39:58 +0100

    cloudinary done

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,8 @@
 class Experience < ApplicationRecord
   belongs_to :user
   belongs_to :solution
+
+  has_one_attached :photo
+
+  validates :username, :rating, :user_id, :solution_id, :content, presence: true
 end
