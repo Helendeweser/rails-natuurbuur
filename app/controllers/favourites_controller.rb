0838a1d3b4baf5120d9c5733343e38f0bd6f1f24
Florian de Radiguès <florian.radigues@gmail.com>
2023-03-09 12:28:53 +0100

    pedro's the best, we stay on the current page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,30 +1,20 @@
 class FavouritesController < ApplicationController
-
   def create
     @favourite = Favourite.new
     @favourite.user = current_user
     @favourite.solution = Solution.find(params[:solution_id])
-
-    # Still have stuff to do
 
     if @favourite.save
       redirect_to root_path, notice: "You have added #{@favourite.solution.title} to your favourites"
     else
       redirect_to root_path, notice: "#{@favourite.solution.title} is already in your favourite"
     end
-
   end
 
   def destroy
     solution = Solution.find(params[:id])
     @favourite = Favourite.where(solution_id: solution.id.to_s).first
     @favourite.destroy
-    redirect_to dashboard_path
+    redirect_back_or_to root_path
   end
-
-  private
-
-  # def favourite_params
-  #   params.require(:favourite).permit
-  # end
 end
