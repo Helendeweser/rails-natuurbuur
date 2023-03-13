7a18e8acd922163e257349681a0080b9dc98d7fd
Florian de Radiguès <florian.radigues@gmail.com>
2023-03-13 15:32:12 +0100

    filtering by diffilculty

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -5,13 +5,7 @@ class SolutionsController < ApplicationController
   def index
     @solutions = Solution.all.order(:title)
 
-    if params[:query].present?
-      sql_query = "title ILIKE :query OR explanation ILIKE :query OR intro ILIKE :query"
-      @solutions = Solution.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @solutions
-    end
-
+    searching_filtering
   end
 
   def show
@@ -24,6 +18,44 @@ class SolutionsController < ApplicationController
 
   def set_favourites
     @favourites = current_user.solutions if user_signed_in?
+  end
+
+  def searching_filtering
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR explanation ILIKE :query OR intro ILIKE :query"
+      @solutions = Solution.where(sql_query, query: "%#{params[:query]}%")
+
+      if params[:solution][:category].present?
+        filter_query = "category in ('{%s}')"
+        @solutions = @solutions.where(filter_query, params[:solution][:category])
+
+        if params[:solution][:difficulty].present?
+          filter_query = "difficulty = ?"
+          @solutions = @solutions.where(filter_query, params[:solution][:difficulty])
+        end
+
+      elsif params[:solution][:difficulty].present?
+        filter_query = "difficulty = ?"
+        @solutions = @solutions.where(filter_query, params[:solution][:difficulty])
+      end
+
+    elsif params[:solution]
+      if params[:solution][:category].present?
+        filter_query = "category in ('{%s}')"
+        @solutions = @solutions.where(filter_query, params[:solution][:category])
+
+        if params[:solution][:difficulty].present?
+          filter_query = "difficulty = ?"
+          @solutions = @solutions.where(filter_query, params[:solution][:difficulty])
+        end
+
+      elsif params[:solution][:difficulty].present?
+        filter_query = "difficulty = ?"
+        @solutions = @solutions.where(filter_query, params[:solution][:difficulty])
+      end
+    else
+      @solutions
+    end
   end
 
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -24,5 +24,4 @@ class Solution < ApplicationRecord
     total = ratings.sum
     total / ratings.size
   end
-
 end
